ce730ec1c7d4a872bd4c51c9f8c9175e41733bff
wanyu7788 <166548951+wanyu7788@users.noreply.github.com>
2024-11-20 17:48:39 -0500

    Update main.py

--- main.py
+++ main.py
@@ -2,26 +2,23 @@ import uvicorn
 from fastapi import FastAPI
 from typing import Union
 from fastapi.middleware.cors import CORSMiddleware
-from fastapi.responses import JSONResponse
 
 app = FastAPI()
 
 origins = ["*"]
 
 app.add_middleware(
-    CORSMiddleware,
-    allow_origins=origins,
-    allow_credentials=True,
-    allow_methods=["*"],
-    allow_headers=["*"],
+  CORSMiddleware,
+  allow_origins=origins,
+  allow_credentials=True,
+  allow_methods=["*"],
+  allow_headers=["*"],
 )
 
 @app.get("/", tags=["Root"])
 def read_root():
-    # 使用 JSONResponse 设置紧凑模式
-    return JSONResponse(content={"Hello": "Wanyu Zhang"}, indent=None, separators=(",", ":"))
+  return {"Hello": "Wanyu Zhang"}
 
 @app.get("/uni/wz2652")
 def read_item(uni_id: str, q: Union[str, None] = None):
-    # 使用 JSONResponse 设置紧凑模式
-    return JSONResponse(content={"uni_id": uni_id, "q": q}, indent=None, separators=(",", ":"))
+  return {"uni_id":wz2652,"q":q}
